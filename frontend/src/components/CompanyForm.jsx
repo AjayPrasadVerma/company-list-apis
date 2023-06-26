42686cfae8621027f6b8c923885823f41e22fbcd
Ajay <imajvermaofficial@gmail.com>
2023-06-26 21:29:12 +0530

    more validation added

--- frontend/src/components/CompanyForm.jsx
+++ frontend/src/components/CompanyForm.jsx
@@ -6,14 +6,58 @@ import {
   redirect,
 } from "react-router-dom";
 import classes from "./CompanyForm.module.css";
+import useInput from "../hooks/use-input";
 
 function CompanyForm({ method, companyData, heading }) {
+  const inputValidate = (value) => value.trim() !== "";
+
   const navigation = useNavigation();
   const navigate = useNavigate();
 
   const cancelHandler = () => {
     navigate("..");
   };
+
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    inputChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+  } = useInput(inputValidate);
+
+  const {
+    value: enteredLocation,
+    isValid: enteredLocationIsValid,
+    hasError: locationInputHasError,
+    inputChangeHandler: locationChangeHandler,
+    inputBlurHandler: locationBlurHandler,
+  } = useInput(inputValidate);
+
+  const {
+    value: enteredAbout,
+    isValid: enteredAboutIsValid,
+    hasError: aboutInputHasError,
+    inputChangeHandler: aboutChangeHandler,
+    inputBlurHandler: aboutBlurHandler,
+  } = useInput(inputValidate);
+
+  let formIsValid = false;
+  if (enteredNameIsValid && enteredLocationIsValid && enteredAboutIsValid) {
+    formIsValid = true;
+  }
+
+  const enteredCompanyName = nameInputHasError
+    ? classes.invalid
+    : classes["form-control"];
+
+  const enteredCompanyLocation = locationInputHasError
+    ? classes.invalid
+    : classes["form-control"];
+
+  const enteredCompanyAbout = aboutInputHasError
+    ? classes.invalid
+    : classes["form-control"];
 
   const isSubmiitting = navigation.state === "submitting";
 
@@ -22,36 +66,63 @@ function CompanyForm({ method, companyData, heading }) {
       <div id={classes["sub-container"]}>
         <h1>{heading}</h1>
         <Form method={method}>
-          <div className={classes["c-info"]}>
+          <div className={enteredCompanyName}>
             <label htmlFor="companyName">Company Name</label>
             <input
               id="companyName"
               type="text"
               name="companyName"
-              required
+              onBlur={nameBlurHandler}
+              onChange={nameChangeHandler}
+              value={enteredName}
               defaultValue={companyData ? companyData.name : ""}
             />
+            {nameInputHasError ? (
+              <p className={classes["error-text"]}>
+                Please enter company name.
+              </p>
+            ) : (
+              ""
+            )}
           </div>
-          <div className={classes["c-info"]}>
+          <div className={enteredCompanyLocation}>
             <label htmlFor="location">Location</label>
             <input
               id="location"
               type="text"
               name="location"
-              required
+              value={enteredLocation}
+              onChange={locationChangeHandler}
+              onBlur={locationBlurHandler}
               defaultValue={companyData ? companyData.location : ""}
             />
+            {locationInputHasError ? (
+              <p className={classes["error-text"]}>
+                Please enter company location.
+              </p>
+            ) : (
+              ""
+            )}
           </div>
 
-          <div className={classes["c-info"]}>
+          <div className={enteredCompanyAbout}>
             <label htmlFor="about">About</label>
             <textarea
               id="about"
               name="about"
               rows="5"
-              required
+              value={enteredAbout}
+              onChange={aboutChangeHandler}
+              onBlur={aboutBlurHandler}
               defaultValue={companyData ? companyData.about : ""}
             />
+            {aboutInputHasError ? (
+              <p className={classes["error-text"]}>
+                Please enter About the company.
+              </p>
+            ) : (
+              ""
+            )}
           </div>
           <div className={classes.actions}>
             <button
@@ -61,7 +132,10 @@ function CompanyForm({ method, companyData, heading }) {
             >
               Cancel
             </button>
-            <button disabled={isSubmiitting}>
+            <button
+              className={classes.saveBtn}
+              disabled={isSubmiitting || !formIsValid}
+            >
               {isSubmiitting ? "Submitting..." : "Save"}
             </button>
           </div>
